545c645c24b2290aaa601c969f24756204eae8b7
nishchal <singhnishchal27@gmail.com>
2024-03-14 23:00:47 +0530

    Setup: uploadthing lib, added route for uploadthing;  Feat: upload files, agnecy details UI with update, delete, initializing user functionality

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -3,7 +3,7 @@
 import { clerkClient, currentUser } from "@clerk/nextjs"
 import { db } from "./db"
 import { redirect } from "next/navigation"
-import { User } from "@prisma/client"
+import { Agency, User } from "@prisma/client"
 
 export const getAuthUserDetails = async () => {
     const user = await currentUser()
@@ -165,4 +165,49 @@ export const verifyAndAcceptInvitation = async () => {
         })
         return agency ? agency.agencyId : null
     }
+}
+
+export const updateAgencyDetails = async (
+    agencyId: string,
+    agencyDetails: Partial<Agency>
+) => {
+    const response = await db.agency.update({
+        where: { id: agencyId },
+        data: { ...agencyDetails },
+    })
+    return response
+}
+
+
+export const deleteAgency = async (agencyId: string) => {
+    const response = await db.agency.delete({ where: { id: agencyId } })
+    return response
+}
+
+// initializing user
+export const initUser = async (newUser: Partial<User>) => {
+    const user = await currentUser()
+    if (!user) return
+
+    const userData = await db.user.upsert({
+        where: {
+            email: user.emailAddresses[0].emailAddress,
+        },
+        update: newUser,
+        create: {
+            id: user.id,
+            avatarUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress,
+            name: `${user.firstName} ${user.lastName}`,
+            role: newUser.role || 'SUBACCOUNT_USER',
+        },
+    })
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+        privateMetadata: {
+            role: newUser.role || 'SUBACCOUNT_USER',
+        },
+    })
+
+    return userData
 }
