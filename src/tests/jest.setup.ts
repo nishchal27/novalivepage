7543cfe2f9b41df41e9373bd72e7e94da158ad93
nishchal <singhnishchal27@gmail.com>
2024-09-04 20:53:09 +0530

    Test: added test cases for middleware.ts and Agency-details.tsx component

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom'
+
+// Mock ResizeObserver
+class ResizeObserver {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+  
+  global.ResizeObserver = ResizeObserver;
